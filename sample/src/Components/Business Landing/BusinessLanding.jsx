45e784f5dfbf0af4adebaab3adc9d99b613b8779
DavoodSharifi <sharifidavood743@gmail.com>
2021-12-18 06:42:56 -0800

    Add-Bussines-landing

--- sample/src/Components/Business Landing/BusinessLanding.jsx
+++ sample/src/Components/Business Landing/BusinessLanding.jsx
@@ -1,31 +1,44 @@
 
 import React, { useRef, useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import './business_landing_style.scss';
-import {personData} from './asset/data/data'
-
-import Card from "./Card/Card";
-
-// import "swiper/css";
-// import "swiper/css/effect-creative"
-import 'swiper/swiper-bundle.min.css'
-import 'swiper/swiper.min.css';
-
+import {personData} from './asset/data/data';
 import Button from './Button/Button'; 
+import Card from "./Card/Card";
+import './business_landing_style.scss';
+
+import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
+import 'swiper/swiper.scss'; // core Swiper
+//import 'swiper/modules/effect-creative/effect-creative.min.css';
+import 'swiper/modules/effect-creative/effect-creative.scss';
+import './styles.scss';
 
 
-import SwiperCore, { EffectCreative} from 'swiper';
-//   SwiperCore.use([EffectCreative]);
 
+
+import 'swiper/modules/navigation/navigation.scss'; // Navigation module
+import 'swiper/modules/pagination/pagination.scss'; // Pagination module
+
+
+
+
+import SwiperCore,{ EffectCreative } from 'swiper';
+
+
+
+
+
+
+SwiperCore.use([EffectCreative]);
 
 const BusinessLanding = () => {
+    console.log(EffectCreative);
     return (
         <div className='main-container'>
             <div id='sidebar-area' className='sidebar-area'>
             <div className='sidebar-content'>
                     <div className='text-area'>
                     <h4>What they've said about us</h4>
-                    <p>lorem ipsum dolor set amet et it lorem ipsum dolor set amet et it</p>
+                    <p>lorem ipsum dolor set amet et it </p>
+                    <p>ipsum dolor set amet et it</p>
                     </div>
                   <div className='btn-area'>
                   <Button value={'Contact Us'} classStyle={'test'}/>
@@ -35,29 +48,41 @@ const BusinessLanding = () => {
                 </div>
 
             </div>
-            <div className='slider-area'>
-            <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      {/* <SwiperSlide><Card/></SwiperSlide>
-      <SwiperSlide><Card/></SwiperSlide>
-      <SwiperSlide><Card/></SwiperSlide>
-      <SwiperSlide><Card/></SwiperSlide> */}
+            <div id="slider-area" className='slider-area'>
 
-      {
-          personData.map((data)=>{
-              return(<div>
-                   <SwiperSlide><Card data={data}/></SwiperSlide>
 
-              </div>)
-          })
 
-      }
-      
+            <Swiper pagination
+            grabCursor={true} effect={'creative'} EffectCreative={{"prev": {"shadow": true,"translate": [0,0,-500]},"next": {"translate": ["120%",0,-500]}}} className="mySwiper">
+
+    {
+        personData.map((pdata)=>{
+            return(<div>
+                <SwiperSlide><Card pdata={pdata}/></SwiperSlide>
+            </div>)
+        })
+    }
+
+{/* <SwiperSlide>slide3</SwiperSlide><SwiperSlide>slide5</SwiperSlide> */}
+
+
+
     </Swiper>
+   
+ 
+   
+  
+   
+
+
+
+
+
+        
+
+
+
+
 
             </div>
         </div>
--- sample/src/Components/Business Landing/Card/Card.jsx
+++ sample/src/Components/Business Landing/Card/Card.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import './card_style.scss'
 
-const Card = ({data}) => {
-    console.log(data);
+const Card = ({pdata}) => {
+    console.log(pdata);
     return (
         <div className='person-card'>
             <div className='descript-area'>
             <span>
-                {data.description}
+            
+                {pdata.description}
             </span>
             </div>
             <div className='details-area'>
-                
+                <div className='img-det'>
+                <img src={pdata.img} alt="" />
+                </div>
+                <div className='text-det'>
+                   
+                    <span className='person-job'>{pdata.job}</span>
+                </div>
+                {/* <span className='person-name'>{pdata.name}</span> */}
             </div>
         </div>
     )
